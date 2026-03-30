8a8267be6e50aa53426ead7d8b220b62f5451a32
libinjareo <libinjareo@163.com>
2026-03-30 16:59:40 +0800

    feat：完成arith_expr_evaluator项目的main模块

--- arith_expr_evaluator/src/main.rs
+++ arith_expr_evaluator/src/main.rs
@@ -1,6 +1,41 @@
+/// 这是算术表达式求值器的主要命令行应用程序
+
 // 注册 parsemath 模块
 mod parsemath;
 
+use std::io;
+use parsemath::ast;
+use parsemath::parser::{ParseError,Parser};
+
+// 函数调用解析器并计算表达式
+fn evaluate(expr:String) -> Result<f64,ParseError> {
+    let expr = expr.split_whitespace().collect::<String>(); //移除空格
+    let mut math_parser = Parser::new(&expr)?;
+    let ast = math_parser.parse()?;
+    println!("The generated AST is {:?}", ast);
+
+    Ok(ast::eval(ast)?)
+}
+
+// Main函数从命令行读取算术表达式并显示结果和错误
+// 它调用evaluate函数来执行计算
 fn main() {
-    println!("Hello, world!");
+    println!("Hello! Welcome to Arithmetic expression evaluator.");
+    println!("You can calculate value for expression such as 2*3+(4-5)+2^3/4. ");
+    println!("Allowed numbers: positive, negative and decimals.");
+    println!("Supported operations: Add, Subtract, Multiply, Divide, PowerOf(^). ");
+    println!("Enter your arithmetic expression below:");
+
+    loop {
+        let mut input = String::new();
+        match io::stdin().read_line( & mut input ) {
+            Ok(_) => {
+                match evaluate(input) {
+                    Ok(val) => println!("The computed number is {:?}", val),
+                    Err(_) => println!("Error in evaluating expression,Please enter valid expression\n"),
+                };
+            }
+            Err(error) => println!("error: {}", error)
+        }
+    }
 }
--- arith_expr_evaluator/src/parsemath/ast.rs
+++ arith_expr_evaluator/src/parsemath/ast.rs
@@ -1,3 +1,5 @@
+use std::error;
+
 /// 此程序包含了可构建的合法抽象语法树节点列表，并且还能够对抽象语法树进行评估以计算出一个值。
 
 
@@ -12,4 +14,41 @@ pub enum Node {
     Caret(Box<Node>, Box<Node>),
     Negative(Box<Node>),
     Number(f64)
+}
+
+// 给定AST,计算数值
+pub fn eval(expr:Node) -> Result<f64,Box<dyn error::Error>> {
+    use Node::*;
+    match expr {
+        Number(i) => Ok(i),
+        Add(expr1,expr2) => Ok(eval(*expr1)? + eval(*expr2)?),
+        Subtract(expr1,expr2) => Ok(eval(*expr1)? - eval(*expr2)?),
+        Multiply(expr1,expr2) => Ok(eval(*expr1)? * eval(*expr2)?),
+        Divide(expr1,expr2) => Ok(eval(*expr1)? / eval(*expr2)?),
+        Caret(expr1,expr2) => Ok(eval(*expr1)?.powf( eval(*expr2)?)),
+        Negative(expr1) => Ok(-eval(*expr1)?),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_expr1() {
+        use crate::parsemath::parser::Parser;
+
+        let ast = Parser::new("1+2-3").unwrap().parse().unwrap();
+        let value = eval(ast).unwrap();
+        assert_eq!(value, 0.0);
+    }
+
+    #[test]
+    fn test_expr2() {
+        use crate::parsemath::parser::Parser;
+
+        let ast = Parser::new("3+2-1*5/4").unwrap().parse().unwrap();
+        let value = eval(ast).unwrap();
+        assert_eq!(value, 3.75);
+    }
 }
